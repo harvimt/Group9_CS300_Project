6e7d5db10f2767f4e4d55dc2ab4f1b71b914fec7
Mark Harviston <harvimt@pdx.edu>
2011-02-22 01:29:09 -0800

    initializeDB working and tested.

--- src/control/ChocAnApp.java
+++ src/control/ChocAnApp.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.jdesktop.application.SingleFrameApplication;
 
@@ -40,43 +41,51 @@ public class ChocAnApp {
 		return db_connection;
 	}
 	
-	static private void initializeDB(){
-		try{
-			Connection conn = ChocAnApp.getConnection();
-			java.sql.Statement stmt = conn.createStatement();
-			
-						
-			final String [] entities = {"member_statuses","members","providers","services","services_rendered"};
-			for(String entity : entities){
-				BufferedReader reader = new BufferedReader(new FileReader("sql/" + entity + ".sql"));
-				String line;
-	            while ((line = reader.readLine()) != null) {
-	            	stmt.addBatch(line);
-	            }
-			}
-			stmt.executeBatch();
-			
-			PreparedStatement pstmt = conn.prepareStatement("INSERT INTO member_statuses (status_name) VALUES (?)");
-			for(MemberStatus status : MemberStatus.values()){
-				pstmt.setString(1, status.name());
-				pstmt.executeUpdate();
-				ResultSet rs = pstmt.getGeneratedKeys();
-				rs.next();
-				status.setStatusId(rs.getInt("status_id"));
-			}
-			
-			
-			
-		}catch(Exception ex){
-			
+	static protected void initializeDB() throws Exception{
+		Connection conn = ChocAnApp.getConnection();
+		java.sql.Statement stmt = conn.createStatement();
+		
+		//CREATE tables
+		final String [] entities = {"member_statuses","members","providers","services","services_rendered"};
+		for(String entity : entities){
+			BufferedReader reader = new BufferedReader(new FileReader("sql/" + entity + ".sql"));
+			StringBuilder builder = new StringBuilder();
+			String line;
+            while ((line = reader.readLine()) != null) {
+            	builder.append(line);
+            	builder.append('\n');
+            }
+            stmt.executeUpdate(builder.toString());
 		}
+		
+		//INSERT MemberStatus enum information into member_statuses (may already exist)
+		PreparedStatement pstmt = conn.prepareStatement("INSERT INTO member_statuses (status_name) VALUES (?)");
+		for(MemberStatus status : MemberStatus.values()){
+			//Note not using batch so as to avoid going through the list twice.
+			pstmt.setString(1, status.name());
+			pstmt.addBatch();
+		}
+		
+		conn.setAutoCommit(false);
+		try {
+			pstmt.executeBatch(); //Execution will fail if they already exist, in which case carry on.
+		}catch (SQLException ex){}
+		conn.commit();
+		conn.setAutoCommit(true);
+		
+		ResultSet rs = stmt.executeQuery("SELECT status_id, status_name FROM member_statuses");
+		while(rs.next()){
+			MemberStatus status = MemberStatus.valueOf(rs.getString("status_name"));
+			status.setStatusId(rs.getInt("status_id"));
+		}
+		
 	}
 
 	/**
      *  Entry point into the UI 
      */
 	public static void main(String args[]) {
-		initializeDB();
+		//initializeDB();
 	}
 
 }
