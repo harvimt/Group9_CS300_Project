29047e634051f07186a3ca891175a47487457438
Mark Harviston <harvimt@pdx.edu>
2011-02-22 19:44:01 -0800

    changed to using a varchar instead of a decimal/floating point number

--- sql/services.sql
+++ sql/services.sql
@@ -6,6 +6,6 @@
 
 CREATE TABLE IF NOT EXISTS services (
   service_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  service_name varchar(32) NOT NULL UNIQUE ,
-  fee decimal NOT NULL  DEFAULT 0
+  service_name varchar(32) NOT NULL UNIQUE,
+  fee varchar(20) NOT NULL  DEFAULT 0
 );
