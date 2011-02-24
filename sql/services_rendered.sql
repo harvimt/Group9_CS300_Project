f9ca95878d450e1db4ff48962d9a77ae50280f96
Mark Harviston <harvimt@pdx.edu>
2011-02-23 17:32:14 -0800

    Added REFERENCES clause (foreign keys) back to services_rendered table the problem was the bad search by members SQL not the Foreign Keys

--- sql/services_rendered.sql
+++ sql/services_rendered.sql
@@ -6,11 +6,11 @@
 
 CREATE TABLE IF NOT EXISTS services_rendered (
   transaction_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  service_logged int NOT NULL DEFAULT (date('now')),
-  service_provided int NOT NULL ,
-  provider_id int NOT NULL,
-  service_id int NOT NULL,
-  member_id int NOT NULL,
+  service_logged INTEGER NOT NULL DEFAULT (date('now')), --datetime
+  service_provided INTEGER NOT NULL, --date
+  provider_id INTEGER NOT NULL REFERENCES providers,
+  service_id INTEGER NOT NULL REFERENCES services,
+  member_id INTEGER NOT NULL REFERENCES members,
   comments varchar(100) NOT NULL,
   fee decimal NOT NULL  DEFAULT 0
 );
