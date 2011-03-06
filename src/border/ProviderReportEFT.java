cfc110e0c9b4c597920d1d334ef983dbc0c20175
Mark Harviston <harvimt@pdx.edu>
2011-03-05 20:02:44 -0800

    Started ProviderReportEFT

--- src/border/ProviderReportEFT.java
+++ src/border/ProviderReportEFT.java
@@ -1,5 +1,10 @@
 package border;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.MessageFormat;
+
 import control.ProviderReport;
 
 /**
@@ -16,7 +21,8 @@ public class ProviderReportEFT {
 	//
 	// Constructors
 	//
-	public ProviderReportEFT() {
+	public ProviderReportEFT(ProviderReport report) {
+		this.report = report;
 	};
 
 	//
@@ -49,5 +55,35 @@ public class ProviderReportEFT {
 	//
 	// Other methods
 	//
+	
+	public void writeEFTRecords(){
+		String filename = MessageFormat.format(
+			"eft/{1,date,yyyy-MM-dd}-{2,date,yyyy-MM-dd}.txt",
+			new Object[]{
+				report.getFrom(),
+				report.getTo()
+			}
+		);
+		MessageFormat EFTFormat = new MessageFormat(
+			"Tansfer {0,number,currency} to {1}"
+		);
+		
+		try{
+			BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+			
+			for(ProviderReport.ReportItem item : report.getReportData()){
+				writer.write(EFTFormat.format(new Object[]{
+					item.total,
+					item.provider.getProviderName()
+				}));
+				writer.write('\n');
+				
+			}
+			
+			writer.close();
+		}catch(IOException ex){
+			System.out.println("Failed to write file");
+		}
+	}
 
 }
