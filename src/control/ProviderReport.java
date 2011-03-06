19ff3d34b4d65c3ca3f3e046c1217e511d6d50ca
Mark Harviston <harvimt@pdx.edu>
2011-03-05 19:20:38 -0800

    ProviderReportEmail created and tested

--- src/control/ProviderReport.java
+++ src/control/ProviderReport.java
@@ -74,8 +74,6 @@ public class ProviderReport {
 				total
 			));
 		}
-		
-		
 	}
 	
 	//
--- src/tests/ReportTest.java
+++ src/tests/ReportTest.java
@@ -1,6 +1,10 @@
 package tests;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.text.DecimalFormat;
 import java.util.Date;
 
 import junit.framework.Assert;
@@ -8,6 +12,8 @@ import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import border.ProviderReportEmail;
 
 import control.MemberReport;
 import control.ProviderReport;
@@ -64,10 +70,12 @@ public class ReportTest extends ServiceRenderedTest{
 			if(item.member.equals(member1)){
 				Assert.assertEquals(sr_arr[0], item.services.get(0));
 				Assert.assertEquals(sr_arr[1], item.services.get(1));
+				Assert.assertEquals(2, item.services.size());
 				Assert.assertEquals(item.total, sr_arr[0].getFee().add(sr_arr[1].getFee()));
 			}else if(item.member.equals(member2)){
 				Assert.assertEquals(sr_arr[2], item.services.get(0));
 				Assert.assertEquals(sr_arr[3], item.services.get(1));
+				Assert.assertEquals(2, item.services.size());
 				Assert.assertEquals(item.total, sr_arr[2].getFee().add(sr_arr[3].getFee()));
 			}else{
 				throw new Exception("Whoops");
@@ -88,14 +96,85 @@ public class ReportTest extends ServiceRenderedTest{
 			if(item.provider.equals(provider1)){
 				Assert.assertEquals(sr_arr[0], item.services.get(0));
 				Assert.assertEquals(sr_arr[2], item.services.get(1));
+				Assert.assertEquals(2, item.services.size());
 				Assert.assertEquals(item.total, sr_arr[0].getFee().add(sr_arr[2].getFee()));
 			}else if(item.provider.equals(provider2)){
 				Assert.assertEquals(sr_arr[1], item.services.get(0));
 				Assert.assertEquals(sr_arr[3], item.services.get(1));
+				Assert.assertEquals(2, item.services.size());
 				Assert.assertEquals(item.total, sr_arr[1].getFee().add(sr_arr[3].getFee()));
 			}else{
 				throw new Exception("Whoops");
 			}
 		}
+	}
+	
+	@Test 
+	public void testProviderReportEmail() throws Exception {
+		ProviderReport report = new ProviderReport(df.parse("Jan 01, 2011"), df.parse("Jan 10, 2011"));
+		
+		report.runReport();
+		
+		ProviderReportEmail emailer = new ProviderReportEmail(report);
+		
+		report = emailer.getReport(); //added for code-coverage
+		emailer.setReport(report); //added for code-coverage
+		
+		emailer.sendEmails();
+		
+		final String filename1 = 
+			"emails/" + 
+			new DecimalFormat("000000000").format(provider1.getProviderId()) +
+			"-2011-01-01-2011-01-10.txt";
+		
+		final String filename2 =
+			"emails/" +
+			new DecimalFormat("000000000").format(provider2.getProviderId()) +
+			"-2011-01-01-2011-01-10.txt";
+		
+		final String[] expected_file1 = new String[]{
+			"To: " + provider1.getEmail(),
+			"From: noreply@chocan.example.org",
+			"Subject: Receipt for services provided from 1/1/11 to 1/10/11",
+			"--",
+			"01/01/2011 | $250.25 | Provided Therapy for John Doe",
+			"01/03/2011 | $100.30 | Provided Massage for Jane Doe",
+			"",
+			"",
+			"Total: $350.55"
+		};
+		
+		final String[] expected_file2 = new String[]{
+			"To: " + provider2.getEmail(),
+			"From: noreply@chocan.example.org",
+			"Subject: Receipt for services provided from 1/1/11 to 1/10/11",
+			"--",
+			"01/02/2011 | $100.30 | Provided Massage for John Doe",
+			"01/04/2011 | $250.25 | Provided Therapy for Jane Doe",
+			"",
+			"",
+			"Total: $350.55"
+		};
+		
+		BufferedReader reader;
+		String line;
+		int i;
+		
+		reader = new BufferedReader(new FileReader(filename1));
+		i=0;
+		while((line = reader.readLine()) != null){
+			Assert.assertEquals(expected_file1[i], line);
+			i+=1;
+		}
+		
+		reader = new BufferedReader(new FileReader(filename2));
+		i=0;
+		while((line = reader.readLine()) != null){
+			Assert.assertEquals(expected_file2[i], line);
+			i+=1;
+		}
+		
+		new File(filename1).delete();
+		new File(filename2).delete();
 	}
 }
