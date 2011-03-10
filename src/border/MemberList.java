f1f8f949986809b463f937b9e2267f989faa2973
Brandon <myonebuddy@gmail.com>
2011-03-09 17:13:12 -0800

    logserviceform reformatted

--- src/border/MemberList.java
+++ src/border/MemberList.java
@@ -537,7 +537,7 @@ public class MemberList extends javax.swing.JFrame {
 				member.delete();
 
 			} catch (Exception e) {
-				JOptionPane.showMessageDialog(this, "Failed to delete Provider", "error", ERROR);
+				JOptionPane.showMessageDialog(this, "Failed to delete Member", "error", ERROR);
 			}
 			reloadData();
 		}
